f2cc6a774f821111a0f75342cb4d3a8944ad989a
curefate <66023115+curefate@users.noreply.github.com>
2026-02-09 13:03:02 +0100

    + GoldManager, BluePrintTray

---
+++ Assets/Scripts/UI/BluePrintTray.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BluePrintTray : MonoBehaviour
+{
+    public GameObject bluePrintPrefab;
+
+    void Update()
+    {
+        if (transform.childCount == 0 && bluePrintPrefab != null)
+        {
+            Instantiate(bluePrintPrefab, transform);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GoldManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GoldManager : MonoBehaviour
+{
+    [SerializeField] private int gold;
+    public int Gold
+    {
+        get => gold;
+        set
+        {
+            gold = value;
+        }
+    }
+    public bool increaseGold;
+    public float increaseInterval;
+    public int increaseAmount;
+
+    private float _timer;
+
+    void Update()
+    {
+        if (increaseGold)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= increaseInterval)
+            {
+                Gold += increaseAmount;
+                _timer = 0f;
+            }
+        }
+    }
+}
